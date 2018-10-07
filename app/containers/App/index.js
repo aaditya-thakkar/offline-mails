4944ef5c956aaaa6edafd0a9e3935e816108b498
aaditya-thakkar <thakkar.aadi1@gmail.com>
2018-10-08 01:06:03 +0530

    [initial]: removed unnecessary files in containers

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,21 +1,8 @@
-/**
- *
- * App
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- */
-
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
-import { Switch, Route } from 'react-router-dom';
 
-import HomePage from 'containers/HomePage/Loadable';
-import FeaturePage from 'containers/FeaturePage/Loadable';
-import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
+import HomePage from 'containers/HomePage';
 
 const AppWrapper = styled.div`
   max-width: calc(768px + 16px * 2);
@@ -29,19 +16,10 @@ const AppWrapper = styled.div`
 export default function App() {
   return (
     <AppWrapper>
-      <Helmet
-        titleTemplate="%s - React.js Boilerplate"
-        defaultTitle="React.js Boilerplate"
-      >
-        <meta name="description" content="A React.js Boilerplate application" />
+      <Helmet titleTemplate="%s - OMG" defaultTitle="Offline Mails Grabber">
+        <meta name="description" content="Offline Mails Grabber" />
       </Helmet>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/features" component={FeaturePage} />
-        <Route path="" component={NotFoundPage} />
-      </Switch>
-      <Footer />
+      <HomePage />
     </AppWrapper>
   );
 }
