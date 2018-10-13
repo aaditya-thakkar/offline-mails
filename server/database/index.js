537d0d0ae5dcc8349fead1a153f3d1ac3f43faee
aaditya-thakkar <thakkar.aadi1@gmail.com>
2018-10-13 14:39:24 +0530

    [otpVerificaiton]: /verifyOtp api integrated

--- server/database/index.js
+++ server/database/index.js
@@ -8,6 +8,7 @@ const app = express();
 const port = 8082;
 
 app.use((req, res, next) => {
+  res.header('content-type', 'application/json');
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header(
